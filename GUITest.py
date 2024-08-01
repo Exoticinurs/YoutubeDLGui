0da413367105403b02f3269ca11180c0e3b6fe3e
Exotic <81514603+Exoticinurs@users.noreply.github.com>
2024-08-01 05:13:52 +0200

    Added GUITEST.py fixed
    Fixed on Linux, too lazy to create new branch.
    Works, no error handling or error windows yet.
    No progressbar either

--- GUITest.py
+++ GUITest.py
@@ -2,25 +2,40 @@ import customtkinter
 import changeDir
 import downloadFunc
 
-def updateDirNameHeader():
-    global labelwithfile
-    changeDir.changeDownloadDir()
-    print(changeDir.directory)
-    labelwithfile.configure(text="Folder Opened: "+changeDir.directory)
 
+def updateOptionMenu(choice):
+    global labelwithtype
+    labelwithtype.configure(text=f"Selected type: {choice}")
+    print(choice)
+
+
+def updateDirNameHeader():
+    global labelwithdir
+    changeDir.changeDownloadDir()
+    labelwithdir.configure(text="Folder Opened: "+changeDir.directory)
+
+def downloadFile(link):
+    global labelwithdir
+    filetype = labelwithdir.cget("text")
+    if filetype == "Audio":
+        downloadFunc.downloadAudio(link)
+    elif filetype == "Video":
+        downloadFunc.downloadVideo(link)
+    else:
+        downloadFunc.downloadAudio(link)
 
 
 def main():
+        global labelwithtype
+        global labelwithdir
 
-        global labelwithfile
-        
         customtkinter.set_appearance_mode("system")
         customtkinter.set_default_color_theme("blue")
 
         root = customtkinter.CTk()
         root.geometry("500x220")
         root.resizable(False, False)
-        root.wm_iconbitmap(r'D:\Pictures\youtubeBLUEFINAL.ico')
+        # root.wm_iconbitmap(r'D:\Pictures\youtubeBLUEFINAL.ico')
         root.wm_title("Youtube Downloader")
 
         frame = customtkinter.CTkFrame(master=root)
@@ -35,14 +50,14 @@ def main():
         buttonsearch = customtkinter.CTkButton(master=frame, text="Select!", command=updateDirNameHeader)
         buttonsearch.place(x=10, y=45)
         
-        dropdown = customtkinter.CTkOptionMenu(master=frame, values= ['Audio', 'Video'], width=140)
+        dropdown = customtkinter.CTkOptionMenu(master=frame, values= ['Audio', 'Video'], width=140, command=updateOptionMenu)
         dropdown.place(x=10, y=80)
                                                 #Trying to get text type from values list.. not needed
-        labelwithtype = customtkinter.CTkLabel(master=frame, text=f"Selected Type: {dropdown.getvar}")
+        labelwithtype = customtkinter.CTkLabel(master=frame, text=f"Selected Type: ")
         labelwithtype.place(y=80,x=155)
         
         
-        button = customtkinter.CTkButton(master=frame, text="Download!", command="", hover_color="#0932b0", width= 300)
+        button = customtkinter.CTkButton(master=frame, text="Download!", command=lambda: downloadFile(entry1.get()), hover_color="#0932b0", width= 300)
         button.place(x=40, y=140)
         
         root.mainloop()
